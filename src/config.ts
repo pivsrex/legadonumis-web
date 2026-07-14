50679f7580f158237c3f9c104d218a8edf3ceb05
PivsRex <ahcoloma+ai@pm.me>
2026-07-14 02:21:08 -0500

    Actualizar versión a 1.0.3

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const CURRENT_VERSION = '1.0.2'
+export const CURRENT_VERSION = '1.0.3'
 export const BUY_URL = 'https://legadonumis.lemonsqueezy.com/checkout/buy/fbc0bc5f-e323-44a6-b007-9fe0cb707efa'
 export const MAC_URL = `https://github.com/pivsrex/Legado-releases/releases/download/v${CURRENT_VERSION}/Legado-${CURRENT_VERSION}-arm64.dmg`
 export const WIN_URL = `https://github.com/pivsrex/Legado-releases/releases/download/v${CURRENT_VERSION}/Legado-Setup-${CURRENT_VERSION}.exe`
